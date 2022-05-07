c9772ff25ce4b59f4a0ab43bee1efa398d351b4c
notweezer123 <efentress@ucsd.edu>
2022-05-06 19:14:51 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,7 +10,6 @@ public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         String[] lines = markdown.split("\n");
-        //Change to file!!!!!!
 
         for (String s: lines){
             if ((s.indexOf("(") != -1) &&
